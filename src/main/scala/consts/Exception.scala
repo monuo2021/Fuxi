7edf58f30ea7667db39492af6e84d7df71d9b6ed
monuo2021 <2363023951@qq.com>
2021-07-30 18:18:17 +0800

    :pencil: update content

--- src/main/scala/consts/Exception.scala
+++ src/main/scala/consts/Exception.scala
@@ -3,20 +3,20 @@ package consts
 import chisel3._
 import chisel3.util.log2Ceil
 
-// exception type
+// exception type，异常类型
 object ExceptType {
   val EXC_TYPE_WIDTH = log2Ceil(11)
 
   val EXC_NONE  = 0.U(EXC_TYPE_WIDTH.W)
-  val EXC_ECALL = 1.U(EXC_TYPE_WIDTH.W)
-  val EXC_EBRK  = 2.U(EXC_TYPE_WIDTH.W)
+  val EXC_ECALL = 1.U(EXC_TYPE_WIDTH.W)   // 机器模式环境调用
+  val EXC_EBRK  = 2.U(EXC_TYPE_WIDTH.W)   // 断点
   val EXC_SRET  = 3.U(EXC_TYPE_WIDTH.W)
-  val EXC_MRET  = 4.U(EXC_TYPE_WIDTH.W)
+  val EXC_MRET  = 4.U(EXC_TYPE_WIDTH.W)   
   val EXC_ILLEG = 5.U(EXC_TYPE_WIDTH.W)   // illegal instruction
   val EXC_IPAGE = 6.U(EXC_TYPE_WIDTH.W)   // instruction page fault
-  val EXC_IADDR = 7.U(EXC_TYPE_WIDTH.W)   // instruction address misaligned
-  val EXC_LOAD  = 8.U(EXC_TYPE_WIDTH.W)
-  val EXC_STAMO = 9.U(EXC_TYPE_WIDTH.W)
+  val EXC_IADDR = 7.U(EXC_TYPE_WIDTH.W)   // instruction address misaligned，指令地址非对齐
+  val EXC_LOAD  = 8.U(EXC_TYPE_WIDTH.W)   // 读存储器访问错误
+  val EXC_STAMO = 9.U(EXC_TYPE_WIDTH.W)   // 写存储器和 AMO 地址/访问非对齐
   val EXC_SPRIV = 10.U(EXC_TYPE_WIDTH.W)  // S-mode instruction
 }
 
@@ -25,11 +25,11 @@ object ExceptCause {
   val EXC_CAUSE_WIDTH = 31
 
   // interruptions
-  val EXC_S_SOFT_INT  = 1.U(EXC_CAUSE_WIDTH.W)
-  val EXC_M_SOFT_INT  = 3.U(EXC_CAUSE_WIDTH.W)
-  val EXC_S_TIMER_INT = 5.U(EXC_CAUSE_WIDTH.W)
+  val EXC_S_SOFT_INT  = 1.U(EXC_CAUSE_WIDTH.W)    // super模式软件中断
+  val EXC_M_SOFT_INT  = 3.U(EXC_CAUSE_WIDTH.W)    // 机器模式
+  val EXC_S_TIMER_INT = 5.U(EXC_CAUSE_WIDTH.W)    // 时钟中断
   val EXC_M_TIMER_INT = 7.U(EXC_CAUSE_WIDTH.W)
-  val EXC_S_EXT_INT   = 9.U(EXC_CAUSE_WIDTH.W)
+  val EXC_S_EXT_INT   = 9.U(EXC_CAUSE_WIDTH.W)    // 外部中断
   val EXC_M_EXT_INT   = 11.U(EXC_CAUSE_WIDTH.W)
 
   // exceptions
