89aa0288af4a141c82cdf93cebfccf28eac59865
monuo2021 <2363023951@qq.com>
2021-07-31 10:45:44 +0800

    :pencil: update content

--- src/main/scala/bpu/PHT.scala
+++ src/main/scala/bpu/PHT.scala
@@ -19,21 +19,21 @@ class PHT extends Module {
   })
 
   // 2-bit saturation counters，2bit饱和计数器
-  val init      = Seq.fill(PHT_SIZE) { "b10".U(2.W) }
+  val init      = Seq.fill(PHT_SIZE) { "b10".U(2.W) }     // PHT_SIZE = 1 << GHR_WIDTH。生成序列，第一个接收Int类型的对象，表示序列的元素个数；第二个是传名参数，接收序列的元素。'b10'表示默认为弱不跳转
   val counters  = RegInit(VecInit(init))
 
   // update counter
-  when (io.lastBranch) {
-    when (counters(io.lastIndex) === "b11".U) {
-      when (!io.lastTaken) {
+  when (io.lastBranch) {    // 如果是分支跳转指令
+    when (counters(io.lastIndex) === "b11".U) {     // 如果是强跳转
+      when (!io.lastTaken) {                        // 如果预测不跳转，则转变为弱跳转
         counters(io.lastIndex) := counters(io.lastIndex) - 1.U
       }
-    } .elsewhen (counters(io.lastIndex) === "b00".U) {
-      when (io.lastTaken) {
+    } .elsewhen (counters(io.lastIndex) === "b00".U) {    // 如果是强不跳转
+      when (io.lastTaken) {                               // 如果预测跳转，则转变为弱不跳转
         counters(io.lastIndex) := counters(io.lastIndex) + 1.U
       }
     } .otherwise {
-      // counter === b01 || counter === b10
+      // counter === b01（弱不跳转） || counter === b10（弱跳转）
       when (!io.lastTaken) {
         counters(io.lastIndex) := counters(io.lastIndex) - 1.U
       } .otherwise {
@@ -43,5 +43,5 @@ class PHT extends Module {
   }
 
   // generate output
-  io.taken := counters(io.index)(1)
+  io.taken := counters(io.index)(1)     // 判断是否跳转
 }
